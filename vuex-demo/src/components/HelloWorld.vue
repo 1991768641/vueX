124cb28b921a2b2ae693426526006fcec78242bd
丁健 <5322389+ding_jian@user.noreply.gitee.com>
2019-11-11 19:48:30 +0800

    vuex

---
+++ vuex-demo/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+ <div>
+   {{count}} / {{message}}
+
+ <button @click="handleclick">add</button>
+ </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  
+  data () {
+    return {
+      title:'hello'
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    handleclick(){
+      this.$store.commit('increment');
+    }
+  },
+  // computed: mapState(['count'])
+  /* ,
+  computed: {
+    count(){
+      return this.$store.state.count
+    }
+  } */
+  computed: {
+    message(){
+      return this.title+'world'
+    },
+    ...mapState({
+      count:state=>state.count
+    })
+  },
+}
+
+</script>
+<style lang='' scoped>
+</style>
